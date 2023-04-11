64781276666f589eccaa87a5d4b7b32b37fb84b3
addas3 <addas3code@gmail.com>
2023-04-11 20:57:44 +0000

    Fix bug and start to do read me

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Hangman Game
 
-Welcome addas3,
+Hangman Game is a Python terminal game, which runs in the Code Institute moch terminal on Heroku.
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+The game selects a random word from a pre-defined list of words. The player has to guess the word one letter at a time. For each incorrect guess, a part of the "hangman" is drawn on the screen. The player has 6 incorrect guesses before the game is over.
 
-## Reminders
+ [View the live site here](https://game-hangman2.herokuapp.com/#)
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
-
-## Creating the Heroku app
-
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
-
-1. `heroku/python`
-2. `heroku/nodejs`
-
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
-
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
-
-Connect your GitHub repository and deploy as normal.
-
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
-
------
-Happy coding!
+![Mockup](docs/readme_images/mockup.jpg)
--- run.py
+++ run.py
@@ -8,15 +8,20 @@ class Hangman:
         Initializes a Hangman game with a list of possible words.
 
         Parameters:
-        word_list (list): A list of strings representing th possible words in the game.
+        word_list (list): A list of strings representing th possible words
+        in the game.
 
         Attributes:
         word(str): The word to be guessed by the player.
         guesses (list): A list of letters that the player has guessed so far.
-        num_guesses (int): The number of incorrect guesses allowed before the game is over.
-        current_guesses (int): The number of incorrect guesses made by the player so far.
-        display_word (str): The word as it appears to the player, with underscores representing unguessed letters.
-        hangman_states (list): A list of art representations of the hangman as it is being drawn.
+        num_guesses (int): The number of incorrect guesses allowed before the
+        game is over.
+        current_guesses (int): The number of incorrect guesses made by the
+        player so far.
+        display_word (str): The word as it appears to the player, with
+        underscores representing unguessed letters.
+        hangman_states (list): A list of art representations of the hangman as
+        it is being drawn.
         """
         self.word_list = word_list
         self.word = ""
@@ -91,20 +96,22 @@ class Hangman:
         print("\n")
         print("Guess the word before the hangman is fully drawn.")
         print("Remember you have 6 guesses only.")
-        print("Quick Hint: think about fruites.")
+        print("Quick Hint: think about fruits.")
         print("Good Luck!!")
         print("\n")
 
     def word_choose(self):
         """
-        Chooses a word at random from the list of possible words and initializes the display_word attribute.
+        Chooses a word at random from the list of possible words and
+        initializes the display_word attribute.
         """
         self.word = random.choice(self.word_list)
         self.word_display = "_" * len(self.word)
 
     def guess(self):
         """
-        Gets a guess from the player and update the word_display and guesses attributes based on the guess.
+        Gets a guess from the player and update the word_display and guesses
+        attributes based on the guess.
         """
         guess = input("Guess a letter: ")
         if not guess.isalpha():
@@ -124,10 +131,11 @@ class Hangman:
                 print(f"Incorrect Answer. you have {self.guesses_num - self.guesses_current} guesses left.")
                 print(self.states_hangman[self.guesses_current])
                 print(self.word_display)
-    
+
     def play(self):
         """
-        Plays a game of Hangman. Calls the intro, word_choose, and guess methods untill the game is won or lost.
+        Plays a game of Hangman. Calls the intro, word_choose, and guess
+        methods untill the game is won or lost.
         Asks the player if they want to play again after the game is over.
         """
         while True:
@@ -136,34 +144,33 @@ class Hangman:
             while True:
                 self.guess()
                 if self.guesses_current == self.guesses_num:
-                    print("Soory you lost! The word was", self.word)
+                    print("Sorry you lost! The word was", self.word)
                     break
                 elif self.word_display == self.word:
                     print("WOOW Congratulations! You guessed the word!")
                     break
-            print("\n")  # create a space before the prompt for the player to play again
-            play_again = input("Do you want to give your self another try? (y/n):")
+            print("\n")
+            play_again = input("Do you want to give your self another try?"
+                               "(y/n):")
             if play_again.lower() != "y":
                 break
-            print("\n") # create a space after the prompt for the player to play again
-    
+            print("\n")
 
-word_list = [ 'apple',
-    'banana', 
-    'blackberry', 
-    'cherry', 
-    'lemon', 
-    'kiwi', 
-    'orange', 
-    'grapes', 
-    'lime', 
-    'mango', 
-    'peach',
-    'pineapple',
-    'raisins',
-    'strawberry',
-    'watermelon']
+
+word_list = ['apple',
+             'banana',
+             'blackberry',
+             'cherry',
+             'lemon',
+             'kiwi',
+             'orange',
+             'grapes',
+             'lime',
+             'mango',
+             'peach',
+             'pineapple',
+             'raisins',
+             'strawberry',
+             'watermelon']
 game = Hangman(word_list)
 game.play()
-
-
